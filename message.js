2be363328d9b57d645cc70c6ba331dccf611e473
JuditAF <idryscreep@gmail.com>
2024-06-11 12:46:01 +0200

    subo reto2 terminado

--- message.js
+++ message.js
@@ -1,13 +1,13 @@
 
 function message (nombre) {
-    return `¡Este es el Mensaje ${nombre}!`;
+    return `¡Este es el Mensaje ${nombre}! 😊`;
 }
 
 console.log(message("1"));
 
 setTimeout(function () {
 
-    console.log("¡Este es el Mensaje 2!")
+    console.log("¡Este es el Mensaje 2! 🤩")
     console.log(message("3"));
     
     }, 3000);
--- user.js
+++ user.js
@@ -1,12 +1,76 @@
 
+const { log, error } = require('console');
+const fs = require('fs');
+
 let user = {
-    name: " ",
-    surname: " ",
-    age: " ",
+    name: "Martin",
+    surname: "de la Vega",
+    age: 34,
 };
 
-// function readFile (){};
 
-// function writeFile (){};
+function crearFile(user) {
+    setTimeout(function () {
 
-// function toJson (op1, op2, user) {};
+        fs.unlink("user.json", (error) => {
+
+            console.log("Eliminando archivo...");
+
+            if (error) {
+                console.log(`Error: ${error}`);
+            }
+        })
+    }
+
+        , 3000)
+}
+
+crearFile(user);
+
+fs.writeFile("user.json", JSON.stringify(user), (error) => {
+
+    console.log("Escribiendo archivo...");
+
+    if (error) {
+        console.log(`Error: ${error}`);
+    }
+}),
+
+    fs.readFile("./user.json", "utf-8", (error, datosUser) => {
+
+        console.log("Leyendo el archivo...");
+
+        if (!error) {
+            console.log(datosUser);
+        } else {
+            console.log(`Error: ${error}`);
+        }
+    });
+
+
+
+
+// OTRA FORMA DE RESOLVER EL EJERCICIO
+
+// fs.unlink("user.json", (error)=> {
+//     console.log("Eliminando archivo...");
+//     if(error){
+//         console.log(`Error: ${error}`);
+//     }
+// });
+
+// fs.writeFile("user.json", JSON.stringify(user), (error)=> {
+//     console.log("Esribiendo archivo...");
+//     if(error) {
+//         console.log(`Error: ${error}`);
+//     }
+// });
+
+// fs.readFile("user.json", "utf-8", (error, datosUser)=> {
+//     console.log("Leyendo el archivo...");
+//     if(!error) {
+//         console.log(datosUser);
+//     } else {
+//         console.log(`Error: ${error}`);
+//     }
+// });
