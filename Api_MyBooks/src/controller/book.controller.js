31f3018d1a981328596aa45968991ef5dc4f5e3c
JuditAF <idryscreep@gmail.com>
2024-06-20 17:36:14 +0200

    subo reto2 terminado

--- Api_MyBooks/src/controller/book.controller.js
+++ Api_MyBooks/src/controller/book.controller.js
@@ -6,6 +6,7 @@ function getStart(request, response){
     let respuesta = {error: true, codigo: 200, mensaje: 'Punto de inicio'};
     response.send(respuesta);
 };
+
 function getBook(request, response){
     let respuesta;
     if(book != null) {
@@ -15,11 +16,62 @@ function getBook(request, response){
     }
     response.send(respuesta);
 };
-// function getBookParams(request, response){};
 
-// function postBook(){};
-// function putBook(){};
-// function deleteBook(){};
 
-// module.exports = {getStart, getBook, getBookParams, postBook, putBook, deleteBook};
-module.exports = {getStart, getBook};
+function postBook(request, response){
+    let respuesta;
+    console.log(request.body);
+    if ( book === null ) {
+        book = { title: request.body.title,
+                 type: request.body.type,
+                 author: request.body.author,
+                 price: request.body.price,
+                 photo: request.body.photo,
+                 id_book: request.body.id_book,
+                 id_user: request.body.id_user
+        }
+        respuesta = {error:false, codigo: 200, 
+                     mensaje: "Libro Creado", data: book}
+    } else {
+        respuesta = {error:true, codigo: 200, 
+            mensaje: "Libro ya existe", data: book}
+    }
+    response.send(respuesta);
+};
+
+
+function putBook(request, response){
+    let respuesta;
+    if (book != null) {
+        book.title = request.body.title;
+        book.type = request.body.type;
+        book.author = request.body.author;
+        book.price = request.body.price;
+        book.photo = request.body.photo;
+        book.id_book = request.body.id_book;
+        book.id_user = request.body.id_user;
+
+        respuesta = {error:false, codigo: 200, 
+            mensaje: "Libro Actualizado", data: book}
+    } else {
+        respuesta = {error:true, codigo: 200, 
+            mensaje: "el Libro no existe", data: book}
+    }
+    response.send(respuesta);
+};
+
+
+function deleteBook(request, response){
+    let respuesta;
+    if (book != null) {
+        book = null;
+        respuesta = {error:false, codigo: 200, 
+            mensaje: "Libro eliminado", data: book}
+    } else {
+        respuesta = {error:true, codigo: 200, 
+            mensaje: "el Libro no existe", data: book}
+    }
+    response.send(respuesta);
+};
+
+module.exports = {getStart, getBook, getBookParams, postBook, putBook, deleteBook};
--- Api_MyBooks/src/models/book.js
+++ Api_MyBooks/src/models/book.js
@@ -16,3 +16,23 @@ class Book {
 }
 
 module.exports = Book;
+
+// {
+//     "title": "El Guerrero a la Sombra del Cerezo",
+//     "type": "Novela Histórica",
+//     "author": "David D.Gil",
+//     "price": "43.50",
+//     "photo": "https://m.media-amazon.com/images/I/91AOK0vUY+L._AC_UF1000,1000_QL80_.jpg",
+//     "id_book": "356569",
+//     "id_user": "18"
+// }
+
+// {
+//     "title": "Ocho Millones de Dioses",
+//     "type": "Novela Histórica",
+//     "author": "David D.Gil",
+//     "price": "44.50",
+//     "photo": "https://m.media-amazon.com/images/I/81l9hyXn+dL._AC_UF894,1000_QL80_.jpg",
+//     "id_book": "359116",
+//     "id_user": "18"
+// }
